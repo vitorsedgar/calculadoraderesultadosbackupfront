94c2f511d84be4ef3bed028d45103c6ed36637c1
Vitor Edgar da Silva <vitorsedgar@gmail.com>
2020-09-21 15:52:24 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
     baseURL:
         process.env.NODE_ENV === "production"
-            ? process.env.REACT_APP_PRODUCTION_API_URL
+            ? process.env.REACT_APP_DEV_API_URL
             : process.env.REACT_APP_DEV_API_URL,
 });
 
